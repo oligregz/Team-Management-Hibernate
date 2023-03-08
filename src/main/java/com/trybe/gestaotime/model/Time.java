7a6b4ed6f68938c627537fcff5526f905f3e96ac
oligregz <gregorysak90@gmail.com>
2023-03-08 15:58:51 -0300

    feat: attributes, getters and setter implemented in the dtam class

--- src/main/java/com/trybe/gestaotime/model/Time.java
+++ src/main/java/com/trybe/gestaotime/model/Time.java
@@ -1,17 +1,52 @@
 package com.trybe.gestaotime.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Classe Time.
  **/
 
 public class Time {
 
-  /**
-   * Atributos.
-   **/
+  private Long id;
 
-  /**
-   * Métodos.
-   **/
+  private String nome;
+  private List<Jogador> jogadores;
+  private List<Torcedor> torcedores;
+
+  public Time() {
+    super();
+    this.jogadores = new ArrayList<Jogador>();
+    this.torcedores = new ArrayList<Torcedor>();
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public List<Jogador> getJogadores() {
+    return jogadores;
+  }
+
+  public void setJogadores(List<Jogador> jogadores) {
+    this.jogadores = jogadores;
+  }
+
+  public List<Torcedor> getTorcedores() {
+    return torcedores;
+  }
+
+  public void setTorcedores(List<Torcedor> torcedores) {
+    this.torcedores = torcedores;
+  }
+
+  public Long getId() {
+    return id;
+  }
 
 }
