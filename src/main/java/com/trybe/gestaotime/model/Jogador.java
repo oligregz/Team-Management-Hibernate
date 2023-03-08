1031746905409746cd88b704bf0f27de8ad39f8a
oligregz <gregorysak90@gmail.com>
2023-03-08 15:02:41 -0300

    feat: attributes, getters and setter implemented in the dlayer class

--- src/main/java/com/trybe/gestaotime/model/Jogador.java
+++ src/main/java/com/trybe/gestaotime/model/Jogador.java
@@ -6,12 +6,47 @@ package com.trybe.gestaotime.model;
 
 public class Jogador {
 
-  /**
-   * Atributos.
-   **/
+  private Long id;
 
-  /**
-   * Métodos.
-   **/
+  private String nome;
+  private String posicao;
+  private int documento;
+  private String time;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getPosicao() {
+    return posicao;
+  }
+
+  public void setPosicao(String posicao) {
+    this.posicao = posicao;
+  }
+
+  public int getDocumento() {
+    return documento;
+  }
+
+  public void setDocumento(int documento) {
+    this.documento = documento;
+  }
+
+  public String getTime() {
+    return time;
+  }
+
+  public void setTime(String time) {
+    this.time = time;
+  }
+
+  public Long getId() {
+    return id;
+  }
 
 }
