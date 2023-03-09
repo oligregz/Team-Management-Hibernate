eb71fc7f16fac43c835e036f64cb95bb075a747d
oligregz <gregorysak90@gmail.com>
2023-03-09 14:38:49 -0300

    feat: overridden all methods in playerdao class

--- src/main/java/com/trybe/gestaotime/dao/DocumentoDao.java
+++ src/main/java/com/trybe/gestaotime/dao/DocumentoDao.java
@@ -22,6 +22,29 @@ public class DocumentoDao extends GenericDao<Documento, Integer> {
   }
 
   @Override
+  Documento findById(Integer id) {
+    EntityManager em = GenericDao.emf.createEntityManager();
+    em.getTransaction().begin();
+    Documento document = em.find(Documento.class, id);
+    em.remove(document);
+    em.getTransaction().commit();
+    em.close();
+    return document;
+  }
+
+  @Override
+  List<Documento> list() {
+    EntityManager em = GenericDao.emf.createEntityManager();
+    
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<Documento> cq = cb.createQuery(Documento.class);
+    Root<Documento> rootEntry = cq.from(Documento.class);
+    CriteriaQuery<Documento> all = cq.select(rootEntry);
+    TypedQuery<Documento> allQuery = em.createQuery(all);
+    return allQuery.getResultList();
+  }
+
+  @Override
   void update(Documento s) {
     EntityManager em = GenericDao.emf.createEntityManager();
     em.getTransaction().begin();
@@ -38,28 +61,5 @@ public class DocumentoDao extends GenericDao<Documento, Integer> {
     em.remove(document);
     em.getTransaction().commit();
     em.close();
-  }
-
-  @Override
-  Documento findById(Integer id) {
-    EntityManager em = GenericDao.emf.createEntityManager();
-    em.getTransaction().begin();
-    Documento document = em.find(Documento.class, id);
-    em.remove(document);
-    em.getTransaction().commit();
-    em.close();
-    return document;
-  }
-
-  @Override
-  List<Documento> list() {
-    EntityManager em = GenericDao.emf.createEntityManager();
-
-    CriteriaBuilder cb = em.getCriteriaBuilder();
-    CriteriaQuery<Documento> cq = cb.createQuery(Documento.class);
-    Root<Documento> rootEntry = cq.from(Documento.class);
-    CriteriaQuery<Documento> all = cq.select(rootEntry);
-    TypedQuery<Documento> allQuery = em.createQuery(all);
-    return allQuery.getResultList();
   }
 }
--- src/main/java/com/trybe/gestaotime/dao/JogadorDao.java
+++ src/main/java/com/trybe/gestaotime/dao/JogadorDao.java
@@ -1,7 +1,66 @@
 package com.trybe.gestaotime.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import com.trybe.gestaotime.model.Jogador;
 
 public class JogadorDao extends GenericDao<Jogador, Integer> {
+
+  @Override
+  void save(Jogador s) {
+    EntityManager em = GenericDao.emf.createEntityManager();
+    em.getTransaction().begin();
+    em.persist(s);
+    em.getTransaction().commit();
+    em.close();
+  }
+
+  @Override
+  Jogador findById(Integer id) {
+    EntityManager em = GenericDao.emf.createEntityManager();
+    em.getTransaction().begin();
+    Jogador player = em.find(Jogador.class, id);
+    em.remove(player);
+    em.getTransaction().commit();
+    em.close();
+    return player;
+  }
+
+  @Override
+  List<Jogador> list() {
+    EntityManager em = GenericDao.emf.createEntityManager();
+    
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<Jogador> cq = cb.createQuery(Jogador.class);
+    Root<Jogador> rootEntry = cq.from(Jogador.class);
+    CriteriaQuery<Jogador> all = cq.select(rootEntry);
+    TypedQuery<Jogador> allQuery = em.createQuery(all);
+    return allQuery.getResultList();
+  }
+
+  @Override
+  void update(Jogador s) {
+    EntityManager em = GenericDao.emf.createEntityManager();
+    em.getTransaction().begin();
+    em.merge(s);
+    em.getTransaction().commit();
+    em.close();
+  }
+
+  @Override
+  void delete(Integer id) {
+    EntityManager em = GenericDao.emf.createEntityManager();
+    em.getTransaction().begin();
+    Jogador document = em.find(Jogador.class, id);
+    em.remove(document);
+    em.getTransaction().commit();
+    em.close();
+  }
 
 }
