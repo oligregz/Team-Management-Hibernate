c1d5e871f313232249e53cf1ce10ff47cf12a305
oligregz <gregorysak90@gmail.com>
2023-03-11 21:07:54 -0300

    fix checkstyle

--- src/main/java/com/trybe/gestaotime/dao/DocumentoDao.java
+++ src/main/java/com/trybe/gestaotime/dao/DocumentoDao.java
@@ -2,13 +2,14 @@ package com.trybe.gestaotime.dao;
 
 import java.util.List;
 
+import com.trybe.gestaotime.model.Documento;
+
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-import com.trybe.gestaotime.model.Documento;
 
 public class DocumentoDao extends GenericDao<Documento, Integer> {
 
