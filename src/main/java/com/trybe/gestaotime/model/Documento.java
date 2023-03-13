0fd7db2ad6264f008b1144bd9fe6a11929f4c011
oligregz <gregorysak90@gmail.com>
2023-03-13 10:30:48 -0300

    fix: yet fixing relationships

--- src/main/java/com/trybe/gestaotime/model/Documento.java
+++ src/main/java/com/trybe/gestaotime/model/Documento.java
@@ -1,12 +1,9 @@
 package com.trybe.gestaotime.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 /**
@@ -19,41 +16,38 @@ public class Documento {
   @GeneratedValue(strategy =  GenerationType.IDENTITY)
   private Integer id;
 
-  @OneToOne(mappedBy = "documento", cascade = CascadeType.ALL, orphanRemoval = true,
-      fetch = FetchType.LAZY)
-  private Jogador jogador;
-
   private String cpf;
 
   private String numeroCarteiraTrabalho;
+
   private String numeroCbf;
-
-  public String getNumeroCbf() {
-    return numeroCbf;
-  }
-
-  public void setNumeroCbf(String numeroCbf) {
-    this.numeroCbf = numeroCbf;
-  }
 
   public Integer getId() {
     return id;
   }
-
+  
   public String getCpf() {
     return cpf;
   }
-
+  
   public void setCpf(String cpf) {
     this.cpf = cpf;
   }
-
+  
   public String getNumeroCarteiraTrabalho() {
     return numeroCarteiraTrabalho;
   }
-
+  
   public void setNumeroCarteiraTrabalho(String numeroCarteiraTrabalho) {
     this.numeroCarteiraTrabalho = numeroCarteiraTrabalho;
+  }
+  
+  public String getNumeroCbf() {
+    return numeroCbf;
+  }
+  
+  public void setNumeroCbf(String numeroCbf) {
+    this.numeroCbf = numeroCbf;
   }
 
 }
