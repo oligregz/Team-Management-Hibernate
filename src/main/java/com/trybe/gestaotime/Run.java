4aa484b22068adc5818ed0a4c53094b4e988aa4e
oligregz <gregorysak90@gmail.com>
2023-03-17 20:59:40 -0300

    feat: implemnents run

--- src/main/java/com/trybe/gestaotime/Run.java
+++ src/main/java/com/trybe/gestaotime/Run.java
@@ -1,17 +1,30 @@
 package com.trybe.gestaotime;
 
+import com.trybe.gestaotime.dao.JogadorDao;
+import com.trybe.gestaotime.model.Jogador;
+import com.trybe.gestaotime.model.Time;
+
 /**
  * Classe Run.
  */
 
 public class Run {
-
   /**
-   * Atributos.
+   * Essa classe serve para manipulação no banco de dados através
+   * dos metodos de cada model.
    */
+  public static void main(String[] args) {
 
-  /**
-   * Métodos.
-   */
+    Time t1 = new Time();
+    t1.setNome("Taitinga");
+
+    Jogador j1 = new Jogador();
+    j1.setNome("Jailson Pé de Foice");
+    j1.setPosicao("Ponta de Lança");
+    j1.setTime(t1);
+
+    JogadorDao jd = new JogadorDao();
+    jd.deletar(2);
+  }
 
 }
